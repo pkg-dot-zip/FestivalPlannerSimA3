82e8876e153238e6f1574c389d43ad9f328d5d27
Jesse <jm.krijgsman@Student.avans.nl>
2021-02-10 18:41:48 +0100

    Made packages: FestivalPlanner, GUI, Agenda

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("LET'S GOOO");
         System.out.println("This is on the development branch!");
-        System.out.println("Sum: " + sum(5, 5););
+        System.out.println("Sum: " + sum(5, 5));
     }
 
     public static int sum (int a, int b){
