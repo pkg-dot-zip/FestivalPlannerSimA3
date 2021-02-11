2858c450ee4ac7f1856573cb1863028d068ad9a2
Jesse <jm.krijgsman@Student.avans.nl>
2021-02-11 13:11:13 +0100

    MainGUI now extends Application + required methodes

--- src/FestivalPlanner/GUI/MainGUI.java
+++ src/FestivalPlanner/GUI/MainGUI.java
@@ -1,4 +1,20 @@
 package FestivalPlanner.GUI;
 
-public class MainGUI {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainGUI extends Application {
+
+    /**
+     * Starts the MainGUI
+     * @param stage  The stage that is shown on screen
+     * @throws Exception Standard exeption in<a href=”{javafx/application/Application.html”></a>
+     */
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        stage.show();
+    }
+
+
 }
