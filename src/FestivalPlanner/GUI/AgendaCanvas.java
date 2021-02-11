435a6f2bd4e720209f4f823f865ff3d9fa29d750
Jesse <jm.krijgsman@Student.avans.nl>
2021-02-11 14:37:52 +0100

    Added Canvas attribute

--- src/FestivalPlanner/GUI/AgendaCanvas.java
+++ src/FestivalPlanner/GUI/AgendaCanvas.java
@@ -2,13 +2,18 @@ package FestivalPlanner.GUI;
 
 import javafx.scene.canvas.Canvas;
 
-public class AgendaCanvas extends Canvas {
+public class AgendaCanvas {
+
+    private Canvas canvas;
 
     //@TODO Needs Agenda object in constructior
     public AgendaCanvas() {
+        this(1920/3f, 1080/2f);
     }
 
     public AgendaCanvas(double width, double height) {
-        super(width, height);
+
     }
+
+
 }
