86de93c9ff6fbe828e7d4c5e75d5b6193e2dbc3e
Jesse <jm.krijgsman@Student.avans.nl>
2021-02-11 15:17:57 +0100

    Added a lineDraw to draw method

--- src/FestivalPlanner/GUI/AgendaCanvas.java
+++ src/FestivalPlanner/GUI/AgendaCanvas.java
@@ -6,6 +6,9 @@ import javafx.scene.layout.BorderPane;
 import org.jfree.fx.FXGraphics2D;
 import org.jfree.fx.ResizableCanvas;
 
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+
 public class AgendaCanvas {
 
     private BorderPane mainPane;
@@ -19,13 +22,25 @@ public class AgendaCanvas {
     public AgendaCanvas(double width, double height) {
         this.mainPane = new BorderPane();
         this.canvas = new ResizableCanvas(g -> draw(g), this.mainPane);
+        this.canvas.setHeight(height);
+        this.canvas.setWidth(width);
         this.mainPane.setCenter(this.canvas);
         FXGraphics2D graphics = new FXGraphics2D(canvas.getGraphicsContext2D());
         draw(graphics);
     }
 
     private void draw(FXGraphics2D graphics) {
-        graphics.drawLine(100,100,1000,1000);
+        graphics.setTransform(new AffineTransform());
+        graphics.setBackground(Color.white);
+        graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+
+        int sideFraction = 8;
+        int topFraction = 12;
+
+        graphics.translate((canvas.getWidth()/ sideFraction), (canvas.getHeight()/ topFraction));
+
+        graphics.drawLine(0,0,(int)canvas.getWidth(), 0);
+        graphics.drawLine(0,0,0, (int)canvas.getHeight());
     }
 
     public Node buildAgendaCanvas() {
