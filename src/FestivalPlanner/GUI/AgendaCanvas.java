2f2e9eadaa70826c53cb59cf85a9a98dcfe7cfb1
Teun <t.j.tleenders@gmail.com>
2021-02-11 17:05:26 +0100

    replaced dragging with scroll function

--- src/FestivalPlanner/GUI/AgendaCanvas.java
+++ src/FestivalPlanner/GUI/AgendaCanvas.java
@@ -1,8 +1,8 @@
 package FestivalPlanner.GUI;
-import java.awt.geom.*;
+
 import javafx.scene.Node;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.BorderPane;
 import org.jfree.fx.FXGraphics2D;
 import org.jfree.fx.ResizableCanvas;
@@ -18,7 +18,7 @@ public class AgendaCanvas {
 
     //@TODO Needs Agenda object in constructior
     public AgendaCanvas() {
-        this(1920/3f, 1080/2f);
+        this(1920 / 3f, 1080 / 2f);
     }
 
     public AgendaCanvas(double width, double height) {
@@ -29,8 +29,8 @@ public class AgendaCanvas {
         this.canvas.setHeight(height);
         this.canvas.setWidth(width);
 
-        this.canvas.setOnMousePressed(this::onMousePressed);
-        this.canvas.setOnMouseDragged(this::onMouseDragged);
+        this.canvas.setOnScroll(this::setOnScroll);
+
 
         this.mainPane.setCenter(this.canvas);
         FXGraphics2D graphics = new FXGraphics2D(canvas.getGraphicsContext2D());
@@ -49,37 +49,23 @@ public class AgendaCanvas {
 
         graphics.translate(sideOffset, topOffset);
 
-        graphics.drawLine(-sideOffset,0,(int)canvas.getWidth(), 0);
-        graphics.drawLine(0,-topOffset,0, (int)canvas.getHeight());
+        graphics.drawLine(-sideOffset, 0, (int) canvas.getWidth(), 0);
+        graphics.drawLine(0, -topOffset, 0, (int) canvas.getHeight());
         drawTopBar(graphics);
     }
 
-    private void drawTopBar(FXGraphics2D graphics){
-        for(int i = 0; i < 24; i++) { //Later veranderen in van begintijd tot eindtijd
-            graphics.drawString(i + ".00", i * 50+10, -25);
+    private void drawTopBar(FXGraphics2D graphics) {
+        for (int i = 0; i < 24; i++) { //Later veranderen in van begintijd tot eindtijd
+            graphics.drawString(i + ".00", i * 50 + 10, -25);
         }
     }
 
-    private boolean dragging = false;
-    private double xpos;
-    private double ypos;
+    private void setOnScroll(ScrollEvent scrollEvent) {
 
-    private void onMousePressed(MouseEvent e) {
-        this.dragging = true;
-        xpos = e.getX();
-        ypos = e.getY();
-        System.out.println(xpos);
-    }
-
-    private void onMouseDragged(MouseEvent e) {
-//        System.out.println(xpos);
-//        System.out.println("x" + (e.getX()-xpos) + " y" + (e.getY()-ypos));
-        Point2D movePoint = new Point2D.Double(e.getX()-xpos, e.getY()-ypos);
-        cameraTransform.translate(movePoint.getX(), movePoint.getY());
-
-        xpos = e.getX();
-        ypos = e.getY();
+        this.cameraTransform.translate(scrollEvent.getDeltaY() / 1.5, 0);
+        System.out.println(scrollEvent.getDeltaY());
         draw(new FXGraphics2D(this.canvas.getGraphicsContext2D()));
+
     }
 
     public Node buildAgendaCanvas() {
