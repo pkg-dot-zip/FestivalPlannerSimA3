c9ceb9935c651ba61f910af5dff6f9bc32c605fb
Jesse <jm.krijgsman@Student.avans.nl>
2021-02-11 16:15:01 +0100

    Added dragging

--- src/FestivalPlanner/GUI/AgendaCanvas.java
+++ src/FestivalPlanner/GUI/AgendaCanvas.java
@@ -1,7 +1,8 @@
 package FestivalPlanner.GUI;
-
+import java.awt.geom.*;
 import javafx.scene.Node;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import org.jfree.fx.FXGraphics2D;
 import org.jfree.fx.ResizableCanvas;
@@ -13,6 +14,7 @@ public class AgendaCanvas {
 
     private BorderPane mainPane;
     private Canvas canvas;
+    private AffineTransform cameraTransform;
 
     //@TODO Needs Agenda object in constructior
     public AgendaCanvas() {
@@ -20,10 +22,16 @@ public class AgendaCanvas {
     }
 
     public AgendaCanvas(double width, double height) {
+        this.cameraTransform = new AffineTransform();
         this.mainPane = new BorderPane();
-        this.canvas = new ResizableCanvas(g -> draw(g), this.mainPane);
+
+        this.canvas = new ResizableCanvas(this::draw, this.mainPane);
         this.canvas.setHeight(height);
         this.canvas.setWidth(width);
+
+        this.canvas.setOnMousePressed(this::onMousePressed);
+        this.canvas.setOnMouseDragged(this::onMouseDragged);
+
         this.mainPane.setCenter(this.canvas);
         FXGraphics2D graphics = new FXGraphics2D(canvas.getGraphicsContext2D());
         draw(graphics);
@@ -34,13 +42,44 @@ public class AgendaCanvas {
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
 
-        int sideFraction = 8;
-        int topFraction = 12;
+        graphics.setTransform(this.cameraTransform);
 
-        graphics.translate((canvas.getWidth()/ sideFraction), (canvas.getHeight()/ topFraction));
+        int sideOffset = 150;
+        int topOffset = 50;
 
-        graphics.drawLine(0,0,(int)canvas.getWidth(), 0);
-        graphics.drawLine(0,0,0, (int)canvas.getHeight());
+        graphics.translate(sideOffset, topOffset);
+
+        graphics.drawLine(-sideOffset,0,(int)canvas.getWidth(), 0);
+        graphics.drawLine(0,-topOffset,0, (int)canvas.getHeight());
+        drawTopBar(graphics);
+    }
+
+    private void drawTopBar(FXGraphics2D graphics){
+        for(int i = 0; i < 24; i++) { //Later veranderen in van begintijd tot eindtijd
+            graphics.drawString(i + ".00", i * 50+10, -25);
+        }
+    }
+
+    private boolean dragging = false;
+    private double xpos;
+    private double ypos;
+
+    private void onMousePressed(MouseEvent e) {
+        this.dragging = true;
+        xpos = e.getX();
+        ypos = e.getY();
+        System.out.println(xpos);
+    }
+
+    private void onMouseDragged(MouseEvent e) {
+//        System.out.println(xpos);
+//        System.out.println("x" + (e.getX()-xpos) + " y" + (e.getY()-ypos));
+        Point2D movePoint = new Point2D.Double(e.getX()-xpos, e.getY()-ypos);
+        cameraTransform.translate(movePoint.getX(), movePoint.getY());
+
+        xpos = e.getX();
+        ypos = e.getY();
+        draw(new FXGraphics2D(this.canvas.getGraphicsContext2D()));
     }
 
     public Node buildAgendaCanvas() {
