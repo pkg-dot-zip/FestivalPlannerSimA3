e4b87f6968f3b493a665056589c3af551c76ab8f
Jesse <jm.krijgsman@Student.avans.nl>
2021-02-14 11:54:45 +0100

    Changed invalid ascii character " to "

--- src/FestivalPlanner/GUI/MainGUI.java
+++ src/FestivalPlanner/GUI/MainGUI.java
@@ -9,7 +9,7 @@ public class MainGUI extends Application {
     /**
      * Starts the MainGUI.
      * @param stage  The stage that is shown on screen
-     * @throws Exception Standard exception in <a href=”https://docs.oracle.com/javase/8/javafx/api/javafx/application/Application.html”>Application</a>
+     * @throws Exception Standard exception in <a href="https://docs.oracle.com/javase/8/javafx/api/javafx/application/Application.html">Application</a>
      */
     @Override
     public void start(Stage stage) throws Exception {
