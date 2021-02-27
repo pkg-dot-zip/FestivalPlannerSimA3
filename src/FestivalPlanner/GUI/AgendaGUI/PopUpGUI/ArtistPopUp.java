19f609d345a008564442751a621330ab5125c10c
Jason Setiowaty <j.setiowaty@student.avans.nl>
2021-02-27 17:01:53 +0100

    Artist picture + sprite selector (not fully functional yet)
    artist picture + sprite finding logic work. No button that activates this, and it doesnt get sent into the artist adder yet....

--- src/FestivalPlanner/GUI/AgendaGUI/PopUpGUI/ArtistPopUp.java
+++ src/FestivalPlanner/GUI/AgendaGUI/PopUpGUI/ArtistPopUp.java
@@ -7,7 +7,12 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.util.ResourceBundle;
 
 /**
@@ -22,6 +27,8 @@ public class ArtistPopUp extends AbstractCreationPopUp{
     private HBox nameHBox = new HBox();
     private TextField nameField = new TextField();
     //TODO: Add sprite & picture functionality.
+    private BufferedImage artistPicture;
+    private BufferedImage artistSprite;
 
     /**
      * Constructor for the <code>ArtistPopUp</code> class.
@@ -60,6 +67,60 @@ public class ArtistPopUp extends AbstractCreationPopUp{
     @Override
     public void additionalActionHandlingSetup() {
 
+    }
+
+    /**
+     * Opens fileChooser dialog to select Artist Photo
+     * @return absolute path to photo of artist
+     */
+    private String findArtistPicture() {
+        FileChooser fileChooser = new FileChooser();
+        try {
+            fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Artist Photo (.png)", "*.png"));
+            return fileChooser.showOpenDialog(new Stage()).getAbsolutePath();
+        } catch (NullPointerException e) {
+            showExceptionPopUp(e);
+        }
+        return null;
+    }
+
+    /**
+     * Sets artist picture to selected picture.
+     */
+    public void setArtistPicture() {
+        try {
+            File picture = new File(findArtistPicture());
+            this.artistPicture = ImageIO.read(picture);
+        } catch (Exception e) {
+            showExceptionPopUp(e);
+        }
+    }
+
+    /**
+     * Opens fileChooser dialog to select Artist Sprite
+     * @return absolute path to sprite of artist
+     */
+    private String findArtistSprite() {
+        FileChooser fileChooser = new FileChooser();
+        try {
+            fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Artist Sprite (.png)", "*.png"));
+            return fileChooser.showOpenDialog(new Stage()).getAbsolutePath();
+        } catch (NullPointerException e) {
+            showExceptionPopUp(e);
+        }
+        return null;
+    }
+
+    /**
+     * Sets artist sprite to selected sprite.
+     */
+    public void setArtistSprite() {
+        try {
+            File picture = new File(findArtistSprite());
+            this.artistSprite = ImageIO.read(picture);
+        } catch (Exception e) {
+            showExceptionPopUp(e);
+        }
     }
 
     @Override
