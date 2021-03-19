7445acde2885da7f95edcc295aa25c392f775d99
Simon <dutchsimon9@gmail.com>
2021-03-19 12:17:56 +0100

    Added tests for LanguageHandler's string retrieval

---
+++ tests/FestivalPlanner/Util/LanguageHandling/LanguageHandlerTest.java
@@ -0,0 +1,32 @@
+package FestivalPlanner.Util.LanguageHandling;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LanguageHandlerTest {
+
+    private String getMessagesApplyMessage = "Should've returned \"Apply\", but returned another string instead!";
+    private String getMessagesCloseMessage = "Should've returned \"Close\", but returned another string instead!";
+
+    @Test
+    void testGetMessages_withApply_returnsApply() {
+        // Arrange
+        // Act
+        String actualValue = LanguageHandler.getMessages().getString("apply");
+        String returnValue = "Apply";
+        // Assert
+        Assertions.assertEquals(returnValue, actualValue, getMessagesApplyMessage);
+    }
+
+    @Test
+    void testGetMessages_withClose_returnsClose() {
+        // Arrange
+        // Act
+        String actualValue = LanguageHandler.getMessages().getString("close");
+        String returnValue = "Close";
+        // Assert
+        Assertions.assertEquals(returnValue, actualValue, getMessagesCloseMessage);
+    }
+}
