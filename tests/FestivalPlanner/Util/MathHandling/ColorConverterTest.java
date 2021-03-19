4a835ac44a0818eb388cd4662b610a8c36c58dd4
Simon <dutchsimon9@gmail.com>
2021-03-19 11:56:21 +0100

    Fixed names for ColorConverter tests

--- tests/FestivalPlanner/Util/MathHandling/ColorConverterTest.java
+++ tests/FestivalPlanner/Util/MathHandling/ColorConverterTest.java
@@ -13,7 +13,7 @@ class ColorConverterTest {
     private String fromJavaFXToAWTMessage = "The converted color is not the same! It should have returned a proper AWT Color!";
 
     @Test
-    void testFromAwtToJavaFX_with_returns() {
+    void testFromAwtToJavaFX_withBlack_returnsBlack() {
         // Arrange
 
         // Act
@@ -25,7 +25,7 @@ class ColorConverterTest {
     }
 
     @Test
-    void testFromJavaFXToAwt_with_returns() {
+    void testFromJavaFXToAwt_withBlack_returnsBlack() {
         // Arrange
 
         // Act
