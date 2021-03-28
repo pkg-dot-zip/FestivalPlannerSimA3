d3e3166c74013233c1d8366363c0cbbefe052730
Jesse <jm.krijgsman@Student.avans.nl>
2021-03-28 20:36:06 +0200

    Changed stage sizes in TileMap

--- src/FestivalPlanner/Logic/SimulatorPodium.java
+++ src/FestivalPlanner/Logic/SimulatorPodium.java
@@ -3,14 +3,18 @@ package FestivalPlanner.Logic;
 import FestivalPlanner.TileMap.TileLayer;
 import org.jfree.fx.FXGraphics2D;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class SimulatorPodium extends SimulatorObject{
 
     private String locationString;
+    private BufferedImage offImage;
 
     /**
      * Main constructor for SimulatorPodium
@@ -21,15 +25,27 @@ public class SimulatorPodium extends SimulatorObject{
     public SimulatorPodium(Point2D location, int width, int height, double rotation, String name, TileLayer collisionLayer, String locationString) {
         super(location, width, height, rotation, name, collisionLayer);
         this.locationString = locationString;
+
+        try {
+            this.offImage = ImageIO.read(getClass().getResourceAsStream("/Images/Stages/Stage_Off.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void draw(FXGraphics2D g2d) {
         Rectangle2D rectangle = new Rectangle2D.Double(location.getX(), location.getY(), width, height);
-        AffineTransform transform = new AffineTransform();
-        transform.rotate(Math.toRadians(this.rotation), this.location.getX(), this.location.getY());
+        AffineTransform rectangleTransform = new AffineTransform();
+        rectangleTransform.rotate(Math.toRadians(this.rotation), this.location.getX(), this.location.getY());
 
-        g2d.draw(transform.createTransformedShape(rectangle));
+        g2d.draw(rectangleTransform.createTransformedShape(rectangle));
+
+        AffineTransform imageTransform = new AffineTransform();
+        imageTransform.rotate(Math.toRadians(this.rotation), this.location.getX(), this.location.getY());
+        imageTransform.translate(location.getX(), location.getY());
+
+        g2d.drawImage(this.offImage, imageTransform, null);
     }
 
     @Override
