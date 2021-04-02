29cf91c345da7bf8475d5567e1431dcac9000291
Jason <j.setiowaty@student.avans.nl>
2021-04-02 16:59:40 +0200

    Tests for Show OGP2

---
+++ tests/FestivalPlanner/Agenda/ShowTest.java
@@ -0,0 +1,36 @@
+package FestivalPlanner.Agenda;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShowTest {
+
+    @Test
+    void getDuration() {
+        //Arrange
+        Show show = new Show();
+        show.setStartTime(LocalTime.of(12, 50, 40));
+        show.setEndTime(LocalTime.of(12, 51, 20));
+        long expectedDuration = 40;
+        //Act
+        long actualDuration = show.getDuration();
+        //Assert
+        Assertions.assertEquals(expectedDuration, actualDuration, "Show duration doesn't add up");
+    }
+
+    @Test
+    void setAndGetName() {
+        //Arrange
+        Show show = new Show();
+        String expectedName = "Test show";
+        //Act
+        show.setName("Test show");
+        //Assert
+
+        Assertions.assertEquals(expectedName, show.getName(), "Names don't match");
+    }
+
+}
