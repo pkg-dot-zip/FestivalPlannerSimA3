efe6bfa1134b65434f388d86afb74105e306030f
Simon <dutchsimon9@gmail.com>
2021-04-02 15:22:50 +0200

    Cleaned up the Util package
    - Updated documentation a bit.
    - Fixed grammatical errors.
    - Fixed StyleGuide errors.

--- src/FestivalPlanner/Util/JsonHandling/package-info.java
+++ src/FestivalPlanner/Util/JsonHandling/package-info.java
@@ -1,4 +1,6 @@
 /**
- * Contains classes for converting JSON files into usable TileMaps for our software.
+ * Contains classes for converting <i>.JSON</i> files into usable TileMaps for our software.
+ * <p>
+ * For these TileMaps we use <a href="https://www.mapeditor.org/">Tiled</a>.
  */
 package FestivalPlanner.Util.JsonHandling;
